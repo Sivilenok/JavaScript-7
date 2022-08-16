0c23310bb5c42fc2e7915d360281c55d63b86f68
sivilenok <cherepahasivi@gmail.com>
2022-08-16 22:17:16 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,56 +2,70 @@
 
 // Task 1
 
-const colors = ['red', 'green', 'blue'];
-console.log(colors.length);
+const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987];
+fibonacci.forEach((item, index) => {
+   console.log(item);
+})
 
-// Task 2
+function printValue(item, index) {
+    console.log(item);
+}
+fibonacci.forEach(printValue);
 
-const animals = ['monkey', 'dog', 'cat'];
-animals[animals.length - 1];
+// Task 2 
+
+function numerateMembers(item, index) {
+    return 'member ' + (index + 1) + ': ' + item
+}
+const users = ['Darya', 'Masha', 'Denis', 'Vitaliy', 'Polina', 'Anton'];
+const allUsers = users.map(numerateMembers);
+console.log(allUsers);
+
+const allUserss = users.map((item, index) => {
+    return `member ${index + 1}: ${item}`;
+});
+console.log(allUserss);
 
 // Task 3
 
-const numbers = [5, 43, 63, 23, 90];
-numbers.splice(0, 5);
-console.log(numbers);
+const numbers = [7, -4, 32, -90, 54, 32, -21];
+const positiveNumbers = numbers.filter((item, index) => {
+    return item >= 0;
+});
+console.log(positiveNumbers);
 
-delete numbers[0, 5];
-console.log(numbers);
-
-// Task 4
-
-const students = ['Polina', 'Dasha', 'Masha'];
-students.splice(2, 1,  `Borya`);
-console.log(students);
-students.splice(1, 1, `Andrey`);
-console.log(students);
-
-// Task 5 
-
-const cats = ['Gachito', 'Tom', 'Batman'];
-for(let i = 0; i < cats.length; i++) {
-    console.log(cats[i]);
+function isPositiveNumbers(item, index) {
+    return item >= 0;
 }
-console.log(cats);
+const positiveNumberss = numbers.filter(isPositiveNumbers);
+console.log(positiveNumberss);
 
-for(let number of cats) {
-    console.log(number);
+// Task 4 
+
+const fibonaccii = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987];
+const sumFibonaccii = fibonacci.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue;
+});
+console.log(sumFibonaccii);
+
+function getSumFibonaccii(previousValue, currentValue) {
+    return previousValue + currentValue;
 }
-console.log(cats);
+const sumFibonacciii = fibonacci.reduce(getSumFibonaccii);
+console.log(sumFibonacciii);
 
-// Task 6 
+// Task 5
 
-const evenNumbers = [2, 4, 6, 8, 10];
-const oddNumbers = [1, 3, 5, 7, 9];
+const numberss = [5, 9, 13, 24, 54, 10, 13, 99, 1, 5];
+const evenNumber = numberss.find((item) => item >= 0);
+console.log(evenNumber);
 
-const allNumbers = evenNumbers.concat(oddNumbers);
-console.log(allNumbers);
-console.log(allNumbers.indexOf(8));
-
-// Task 7
-
-const binary = [0, 0, 0, 0];
-const foo = binary.join(1);
-console.log(foo);
-
+function getEvenNumber(item, index) {
+    if (item >= 0) {
+        return item;
+    } else {
+        return undefined;
+    }
+}
+const evenNumbers = numberss.find(getEvenNumber);
+console.log(evenNumbers);
